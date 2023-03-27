4bcb589270f38601b8633db090770018710d7bce
Camasaurus <0157863@schoolsnet.act.edu.au>
2023-03-27 12:51:06 +1100

    Andor and test key
    End door implemented with no scene to load to at the moment.
    
    + endlevel.gd

--- Lights Out/Maps/EndLevel.gd
+++ Lights Out/Maps/EndLevel.gd
@@ -5,7 +5,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Area_body_entered(body):
-	if body.name == "AttackOnTitan_Key" or body.name == "Pickable":
+	if body.name == "Player":
 		self.queue_free()
 		body.queue_free()
 		Global.hasClicker = true
+		get_tree().change_scene("res://Maps/Map2/Map2.tscn")
