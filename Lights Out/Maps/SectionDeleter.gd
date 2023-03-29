009d8ecd2d02683e579768b9d06acf5063515761
Ruben <dylanjp@iinet.net.au>
2023-03-29 12:14:45 +1100

    Section Deleting Now Works!
    :D

--- Lights Out/Maps/SectionDeleter.gd
+++ Lights Out/Maps/SectionDeleter.gd
@@ -1,6 +1,9 @@
 extends Area
 export(NodePath) var Section_Path
+export(NodePath) var Blockade_Path
 onready var Section_Node = get_node(Section_Path)
+onready var Blockade_Node = get_node(Blockade_Path)
+var isPlayer = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,5 +19,9 @@ func _ready():
 func _process(delta):
 	if self.get_overlapping_bodies():
 		if self.get_overlapping_bodies()[0].name == "Player":
-			print("ahh shit he out the clpsest")
-			Section_Node.queue_free()
+			if !isPlayer:
+				print("ahh shit he out the clpsest")
+				Section_Node.queue_free()
+				isPlayer = true
+				Blockade_Node.visible = true
+				Blockade_Node.set_use_collision(true)
