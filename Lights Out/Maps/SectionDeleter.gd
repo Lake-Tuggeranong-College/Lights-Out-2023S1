8a6a3d421a914dcc4ed34727d83970b8b00d2754
Ruben <dylanjp@iinet.net.au>
2023-03-29 12:35:37 +1100

    VR section deletion works

--- Lights Out/Maps/SectionDeleter.gd
+++ Lights Out/Maps/SectionDeleter.gd
@@ -3,7 +3,7 @@ export(NodePath) var Section_Path
 export(NodePath) var Blockade_Path
 onready var Section_Node = get_node(Section_Path)
 onready var Blockade_Node = get_node(Blockade_Path)
-var isPlayer = false
+var isPlayerOut = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -17,11 +17,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if self.get_overlapping_bodies():
-		if self.get_overlapping_bodies()[0].name == "Player":
-			if !isPlayer:
-				print("ahh shit he out the clpsest")
-				Section_Node.queue_free()
-				isPlayer = true
-				Blockade_Node.visible = true
-				Blockade_Node.set_use_collision(true)
+	if Global.desktop:
+		for i in self.get_overlapping_bodies():
+			if i.name == "Player":
+				if !isPlayerOut:
+					delteSection()
+	if Global.vr:
+		for i in self.get_overlapping_areas():
+			if i.name == "Player":
+				if !isPlayerOut:
+					delteSection()
+				
+func delteSection():
+	Section_Node.queue_free()
+	isPlayerOut = true
+	Blockade_Node.visible = true
+	Blockade_Node.set_use_collision(true)
