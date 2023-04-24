d1487513e776334fc22af876e257f772bcc0e411
novastick <99231858+novastick@users.noreply.github.com>
2023-04-24 12:52:52 +1000

    the pause menu is real and it works

--- Lights Out/UI/pause_menu/pause_menu.gd
+++ Lights Out/UI/pause_menu/pause_menu.gd
@@ -11,3 +11,11 @@ func _ready():
 	pass # Replace with function body.
 
 
+
+
+func _on_resume_game_pressed():
+		get_tree().change_scene("res://Maps/Map1.tscn")
+
+
+func _on_quit_game_pressed():
+	get_tree().change_scene("res://Launcher.tscn")
