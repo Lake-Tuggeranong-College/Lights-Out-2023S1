8f5c3b3aefe3ae9935ca5c8027e1186f9e0cd5f2
Ruben <dylanjp@iinet.net.au>
2023-05-05 10:27:22 +1000

    Fixed level 1 stuff and VR pause menu

--- Lights Out/addons/godot-openxr/scenes/Pause Menu VR.gd
+++ Lights Out/addons/godot-openxr/scenes/Pause Menu VR.gd
@@ -10,20 +10,19 @@ func _ready():
 
 var is_paused = false setget set_is_paused
 
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_menu"):
-		self.is_paused = !is_paused
 
 func set_is_paused(value):
+	print("awww yeah changiun pasuye")
 	is_paused = value
 	get_tree().paused = is_paused
-	visible = is_paused
-	
+	$"../../ARVRCamera/Viewport2Din3D".visible = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if controller and controller.get_is_active() and controller.is_button_pressed(pause_button):
+	if controller and controller.get_is_active() and controller.is_button_pressed(pause_button) and !isActive:
+		print("hel")
 		isActive = true
+		set_is_paused(true)
 		
-	if controller and controller.get_is_active() and !controller.is_button_pressed(pause_button):
+	if controller and controller.get_is_active() and !controller.is_button_pressed(pause_button) and isActive:
 		isActive = false
-		get_tree().paused = false
