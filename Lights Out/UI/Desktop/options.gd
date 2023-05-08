ebd2de5412057f4f980a60eed8eee245bb139c21
novastick <99231858+novastick@users.noreply.github.com>
2023-05-08 12:48:32 +1000

    UI likes me i guess

--- Lights Out/UI/Desktop/options.gd
+++ Lights Out/UI/Desktop/options.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://UI/Desktop/Main Menu.tscn")
+
+
+func _on_sonar_color_pressed():
+	get_tree().change_scene("res://UI/other/sonar_color.tscn")
