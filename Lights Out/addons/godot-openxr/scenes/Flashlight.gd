fdf166d818bdb8f1faebfe1cb1aa71735d3b6d38
Ruben <dylanjp@iinet.net.au>
2023-05-12 09:13:27 +1000

    Flashlight on Player
    yay :confetti_ball: :100:

--- Lights Out/addons/godot-openxr/scenes/Flashlight.gd
+++ Lights Out/addons/godot-openxr/scenes/Flashlight.gd
@@ -1,10 +1,11 @@
-extends Spatial
+extends SpotLight
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var isActive = false
+var isButtonDown = false
+var lightActive = false
 onready var controller := ARVRHelpers.get_arvr_controller(self)
 export (XRTools.Buttons) var click_button : int = XRTools.Buttons.VR_TRIGGER
 # Called when the node enters the scene tree for the first time.
@@ -14,11 +15,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if controller and controller.get_is_active() and controller.is_button_pressed(click_button) and !isActive and Global.hasClicker:
-		isActive = true
-		if Global.in_area:
-			$"../../OmniLight".visible = true
-			$"../../OmniLight/Timer".start()
-		$AudioStreamPlayer3D.emmit_sound("res://Assets/Audio Files/UIconfirmationOptimistic_SFX.wav")
+	if controller and controller.get_is_active() and controller.is_button_pressed(click_button) and !isButtonDown and !lightActive:
+		isButtonDown = true
+		lightActive = true
+		self.visible = true
+		yield(get_tree().create_timer(5.0), "timeout")
+		self.visible = false
+		yield(get_tree().create_timer(5.0), "timeout")
+		lightActive = false
 	if controller and controller.get_is_active() and !controller.is_button_pressed(click_button):
-		isActive = false
+		isButtonDown = false
