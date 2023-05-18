d9c5b24482c14ca2a2dad4e49e29de210dd7169d
BosstheBIGSHOT <8092007@schoolsnet.act.edu.au>
2023-05-19 09:41:53 +1000

    Key back to original, outlines removed,  closet textures

--- Lights Out/Maps/Outline.gd
+++ Lights Out/Maps/Outline.gd
@@ -1,10 +1,7 @@
 extends MeshInstance
 
 func _input(event):
-	if Input.is_action_just_pressed("left_click"):
-		$Timer.start()
-		self.visible = true;
-		print ("mumba")
+	pass
 
 
 func _on_Timer_timeout():
