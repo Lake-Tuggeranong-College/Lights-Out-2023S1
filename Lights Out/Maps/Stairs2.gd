376d8676dcefaee74afc4efa88876814e03c3371
Ruben <dylanjp@iinet.net.au>
2023-05-22 12:54:36 +1000

    Removed safe and replaced for keypad door

--- Lights Out/Maps/Stairs2.gd
+++ Lights Out/Maps/Stairs2.gd
@@ -9,10 +9,11 @@ extends Spatial
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Global.vr:
-		$CSGMesh.use_collision = true
-		$StaticBody/CollisionShape.disabled = true
+		$StaticBody/VRCollisionShape.disabled = false
+		$StaticBody/DesktopCollisionShape.disabled = true
 	if Global.desktop:
-		$StaticBody/CollisionShape.disabled = false
+		$StaticBody/VRCollisionShape.disabled = true
+		$StaticBody/DesktopCollisionShape.disabled = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
